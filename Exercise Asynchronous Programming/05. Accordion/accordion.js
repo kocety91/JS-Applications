b56f5f8171b827cdff165ce7db3f9dd24562e728
kocety91 <kocety91@gmail.com>
2021-10-26 15:12:20 +0300

    up

--- Exercise Asynchronous Programming/05. Accordion/accordion.js
+++ Exercise Asynchronous Programming/05. Accordion/accordion.js
@@ -1,22 +1,67 @@
-function solution() {
-  (async () => {
+async function solution() {
+  let main = document.querySelector("#main");
+  let responce = await fetch(
+    "http://localhost:3030/jsonstore/advanced/articles/list"
+  );
+  let data = await responce.json();
+
+  data.forEach(async (x) => {
+    let currentArrticle = await createArticle(x._id, x.title);
+    main.appendChild(currentArrticle);
+  });
+
+  async function createArticle(id, title) {
+    let div = document.createElement("div");
+    div.classList.add("accordion");
+
+    let divHead = document.createElement("div");
+    divHead.classList.add("head");
+
+    let span = document.createElement("span");
+    span.textContent = title;
+    let btn = document.createElement("button");
+    btn.classList.add("button");
+    btn.id = id;
+    btn.textContent = "More";
+    btn.addEventListener("click", showMore);
+
+    divHead.appendChild(span);
+    divHead.appendChild(btn);
+
+    let divExtra = document.createElement("div");
+    divExtra.classList.add("extra");
+    let p = document.createElement("p");
+    let data = await getContent(id);
+    p.textContent = data.content;
+
+    divExtra.appendChild(p);
+
+    div.appendChild(divHead);
+    div.appendChild(divExtra);
+
+    return div;
+  }
+
+  async function getContent(id) {
     let responce = await fetch(
-      "http://localhost:3030/jsonstore/advanced/articles/list"
+      `http://localhost:3030/jsonstore/advanced/articles/details/${id}`
     );
     let data = await responce.json();
 
-    console.log(data);
-  })();
+    return data;
+  }
+
+  function showMore(e) {
+    let article = e.target.parentElement.parentElement;
+    let extraDiv = article.lastElementChild;
+    extraDiv.classList.remove("extra");
+    let divHead = article.firstElementChild;
+    let btn = divHead.lastElementChild;
+    btn.textContent = btn.textContent === "More" ? "Less" : "More";
+
+    extraDiv.style.display =
+      extraDiv.style.display === "block" ? "none" : "block";
+  }
 }
 
-{
-  /* <div class="accordion">
-<div class="head">
-    <span>Scalable Vector Graphics</span>
-    <button class="button" id="ee9823ab-c3e8-4a14-b998-8c22ec246bd3">More</button>
-</div>
-<div class="extra">
-    <p>Scalable Vector Graphics .....</p>
-</div>
-</div> */
-}
+window.addEventListener("load", solution);
