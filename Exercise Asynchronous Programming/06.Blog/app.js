baa02bc842c5cd8db7d9d92f44a7cd0cbaefc9be
kocety91 <kocety91@gmail.com>
2021-10-28 17:06:26 +0300

    Exercise: Data and Authentication

--- Exercise Asynchronous Programming/06.Blog/app.js
+++ Exercise Asynchronous Programming/06.Blog/app.js
@@ -1,5 +1,36 @@
 function attachEvents() {
-    console.log('TODO...');
+  document.querySelector("#btnLoadPosts").addEventListener("click", getPosts);
+  document
+    .querySelector("#btnViewPost")
+    .addEventListener("click", viewPotsById);
+}
+
+async function getPosts() {
+  let responce = await fetch("http://localhost:3030/jsonstore/blog/posts");
+  let data = await responce.json();
+
+  await createOptions(data);
+}
+
+async function createOptions(data) {
+  let pots = document.querySelector("#posts");
+  Object.values(data).forEach(async (x) => {
+    let option = document.createElement("option");
+    option.value = x.id;
+    option.textContent = x.title;
+    pots.appendChild(option);
+  });
+}
+
+async function viewPotsById(e) {
+  let select = e.target.previousElementSibling;
+  let id = select.options[select.selectedIndex].value;
+  let responce = await fetch(
+    `http://localhost:3030/jsonstore/blog/comments/${id}`
+  );
+  let data = await responce.json();
+
+  console.log(data);
 }
 
 attachEvents();
