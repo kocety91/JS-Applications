cb3e2df6a87ba443587c0bfd9c5391acdca14ef9
kocety91 <kocety91@gmail.com>
2021-11-05 11:41:51 +0200

    up

--- Exercise Client Side Rendering/03.Search-in-List/search.js
+++ Exercise Client Side Rendering/03.Search-in-List/search.js
@@ -1,3 +1,31 @@
-function search() {
-   // TODO
+import { html, render } from "https://unpkg.com/lit-html?module";
+import { towns } from "./towns.js";
+
+let tid = document.querySelector("#towns");
+let baseTowns = towns.map((t) => ({ name: t }));
+
+let createTemplate = (data) => html`
+  <ul>
+    ${data.map((x) => html`<li class=${x.class}>${x.name}</li>`)}
+  </ul>
+`;
+
+document.querySelector("button").addEventListener("click", findTown);
+
+render(createTemplate(baseTowns), tid);
+
+function findTown() {
+  let input = document.querySelector("#searchText").value.toLowerCase();
+
+  let allTowns = towns.map((x) => ({ name: x }));
+
+  let matchedTowns = allTowns.filter((t) =>
+    t.name.toLowerCase().includes(input)
+  );
+
+  matchedTowns.forEach((t) => (t.class = "active"));
+
+  render(createTemplate(allTowns), tid);
+
+  console.log(allTowns);
 }
